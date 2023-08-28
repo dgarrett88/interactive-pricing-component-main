4e461aaa9e7b011304821372eea81f161f4bc8fb
dgarrett88 <87794124+dgarrett88@users.noreply.github.com>
2023-08-28 21:52:07 +1000

    basic setup

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const customRange = document.querySelector(".custom-range");
+    const customValueLabel = document.getElementById("customValueLabel");
+    const customPrice = document.querySelector(".price-tag");
+  
+    console.log("customRange:", customRange);
+    console.log("customValueLabel:", customValueLabel);
+  
+    const valueLabels = ["10K", "50K", "100K", "500K", "1M"];
+
+    const price = [8, 12, 16, 24, 36 ]
+  
+    customRange.addEventListener("input", () => {
+      const value = parseInt(customRange.value);
+      console.log("Value:", value); // Check if the value is calculated correctly
+      customValueLabel.textContent = valueLabels[value - 1];
+      customPrice.textContent = price[value - 1];
+    });
+  });
+  
