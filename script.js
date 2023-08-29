37d39b86800012bd7ac1d1c9d9de058bddb62ad2
dgarrett88 <87794124+dgarrett88@users.noreply.github.com>
2023-08-29 15:22:11 +1000

    js finished

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const customRange = document.querySelector(".slider");
-    const customValueLabel = document.getElementById("customValueLabel");
-    const customPrice = document.querySelector(".price-tag");
-  
-    console.log("customRange:", customRange);
-    console.log("customValueLabel:", customValueLabel);
-  
-    const valueLabels = ["10K", "50K", "100K", "500K", "1M"];
+// Get DOM elements
+const toggleBox = document.querySelector(".toggle-checkbox");
+const customRange = document.querySelector(".slider");
+const customValueLabel = document.getElementById("customValueLabel");
+const customPrice = document.querySelector(".price-tag");
 
-    const price = [8, 12, 16, 24, 36 ]
-  
-    customRange.addEventListener("input", () => {
-      const value = parseInt(customRange.value);
-      console.log("Value:", value); // Check if the value is calculated correctly
-      customValueLabel.textContent = valueLabels[value - 1];
-      customPrice.textContent = price[value - 1];
-    });
-  });
-  
+// Define values for labels and prices
+const valueLabels = ["10K", "50K", "100K", "500K", "1M"];
+const price = [8, 12, 16, 24, 36];
+
+// Function to update price and labels
+const updatePrice = () => {
+    const value = parseInt(customRange.value);
+    customValueLabel.textContent = valueLabels[value - 1];
+    
+    // Apply discount if toggle is on
+    if (isDiscountApplied) {
+        customPrice.textContent = price[value - 1] * 0.75;
+    } else {
+        customPrice.textContent = price[value - 1];
+    }
+};
+
+// Track whether the discount is applied
+let isDiscountApplied = false;
+
+// Event listener for toggle box click
+toggleBox.addEventListener('click', () => {
+    isDiscountApplied = !isDiscountApplied;
+    updatePrice();
+});
+
+// Event listener for range input change
+customRange.addEventListener("input", updatePrice);
